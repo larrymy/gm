4fe7e7d0549d1038a8900e67da939078ccff9e21
junyitt <junyitt1995@gmail.com>
2017-10-01 01:21:16 +0800

    remove reg route

--- routes/index.js
+++ routes/index.js
@@ -97,26 +97,26 @@ router.get("/admin_uploads", middleware.isLoggedIn, function(req,res){
 	})
 });
 
-// show register form
-router.get("/register", function(req, res){
-   res.render("register"); 
-});
+// // show register form
+// router.get("/register", function(req, res){
+//    res.render("register"); 
+// });
 
-//handle sign up logic
-router.post("/register", function(req, res){
-    var newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, function(err, user){
-        if(err){
-            console.log(err);
-            req.flash("error", err.message);
-            res.redirect ("/register");
-        }
-        passport.authenticate("local")(req, res, function(){
-            req.flash("success", "Welcome to GM Admin Dashboard, " + user.username);
-           res.redirect("/"); 
-        });
-    });
-});
+// //handle sign up logic
+// router.post("/register", function(req, res){
+//     var newUser = new User({username: req.body.username});
+//     User.register(newUser, req.body.password, function(err, user){
+//         if(err){
+//             console.log(err);
+//             req.flash("error", err.message);
+//             res.redirect ("/register");
+//         }
+//         passport.authenticate("local")(req, res, function(){
+//             req.flash("success", "Welcome to GM Admin Dashboard, " + user.username);
+//            res.redirect("/"); 
+//         });
+//     });
+// });
 
 
 
